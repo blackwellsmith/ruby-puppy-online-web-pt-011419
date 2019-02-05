b21a598f0fc6c083a91c412b7935f7c210d2d332
blackwell <blackwellsmithiv@gmail.com>
2019-02-05 14:17:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << name
+    @@all << self
   end
   
   def self.clear_all
@@ -11,7 +11,7 @@ class Dog
   end
   
   def self.all
-    print @@all 
+     @@all 
     
   end
   
